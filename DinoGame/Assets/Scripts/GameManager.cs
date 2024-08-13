40de3dbfec9c173d52f8b300a00e69d6613b42e8
sinemsooplay <sinemkadakall@gmail.com>
2024-08-13 14:25:34 +0300

    Score and hiscore added.The game finished.

--- DinoGame/Assets/Scripts/GameManager.cs
+++ DinoGame/Assets/Scripts/GameManager.cs
@@ -15,7 +15,11 @@ public class GameManager : MonoBehaviour
     private Spawner spawner;
 
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI hiscoreText;
     public Button retryButton;
+
+    private float score;
     private void Awake()
     {
         if (Instance == null)
@@ -56,12 +60,15 @@ public class GameManager : MonoBehaviour
         }
 
         gameSpeed = initialGameSpeed;
+        score = 0f;
         enabled = true;
 
         player.gameObject.SetActive(true);
         spawner.gameObject.SetActive(true);
         gameOverText.gameObject.SetActive(false);
         retryButton.gameObject.SetActive(false);
+
+        UpdateHiscore();
     }
 
 
@@ -75,10 +82,27 @@ public class GameManager : MonoBehaviour
 
         gameOverText.gameObject.SetActive(true);
         retryButton.gameObject.SetActive(true);
+
+        UpdateHiscore();
     }
     private void Update()
     {
         gameSpeed += gameSpeedIncrease * Time.deltaTime;
+        score += gameSpeed * Time.deltaTime;
+        scoreText.text = Mathf.FloorToInt( score).ToString("D5");
+
     }
 
+    private void UpdateHiscore()
+    {
+        float hiscore = PlayerPrefs.GetFloat("hiscore", 0);
+
+        if(score > hiscore)
+        {
+            hiscore = score;
+            PlayerPrefs.SetFloat("hiscore",hiscore);
+        }
+
+        hiscoreText.text = Mathf.FloorToInt(hiscore).ToString("D5");
+    }
 }
